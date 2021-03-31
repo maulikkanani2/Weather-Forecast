8130c25d1f147d2ec2e070900ee1dcce0b247e8b
Maulik <demo.globalia2@gmail.com>
2021-03-31 20:40:09 +0530

    Remove comments

--- report/models.py
+++ report/models.py
@@ -7,14 +7,10 @@ class WeatherInfo(models.Model):
     lon = models.DecimalField(max_digits=8, decimal_places=6)
     timezone = models.CharField(max_length=100, blank=True, null=True)
     timezone_offset = models.CharField(max_length=20, blank=True, null=True)
-    # minutely = JSONField(models.CharField(max_length=100, blank=True, null=True))
-    # hourly = JSONField(models.CharField(max_length=100, blank=True, null=True))
-    # daily = JSONField(models.CharField(max_length=100, blank=True, null=True))
     minutely = JSONField(null=True, blank=True)
     hourly = JSONField(null=True, blank=True)
     daily = JSONField(null=True, blank=True)
     date = models.DateTimeField(auto_now=True)
-
 
     class Meta:
         db_table = "weather"
--- report/views.py
+++ report/views.py
@@ -2,43 +2,25 @@ import requests
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 from .models import WeatherInfo
-from django.db import transaction
-from django.db import DatabaseError
-import django
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from .serializers import WeatherSerializer
-from django.views.decorators.csrf import csrf_exempt
 
-@api_view()
-@permission_classes([AllowAny])
+
 def store_data(request):
 
-    # print("lat",lat)
-    # print("lon",request.GET.get('lon'))
-    # print("detailing", detailing)
     lat = request.GET['lat']
-    lon = request.query_params['lon']
-    detailing = request.query_params['detailing']
-    print("lat",lat)
-    print("lon", lon)
-    print("detailing",detailing)
+    lon = request.GET['lon']
+    detailing = request.GET['detailing']
     url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude='+detailing+'&appid' \
         '=676d95b33a0d70d28ff022f4cf88efb6'
     response = requests.get(url)
     api_data = response.json()
-
-    # minutely = api_data['minutely']
-    # item = next(item['dt'] for item in minutely if 'dt' in item)
-    # print("item",item)
-
     already_has_data = WeatherInfo.objects.filter(lat=lat, lon=lon)
-    print("already_has_data",len(already_has_data))
+
     if len(already_has_data) > 0:
         print("data already store in db")
     else:
-        # try:
-        print("else insertdata")
         insert_data = WeatherInfo(
             lat=lat,
             lon=lon,
@@ -50,20 +32,13 @@ def store_data(request):
         )
         insert_data.save()
 
-        # except:
-        #     pass
-
-    # queryset = WeatherInfo.objects.all()
     serializer = WeatherSerializer(already_has_data, many=True)
     return JsonResponse(serializer.data, safe=False)
 
-    return HttpResponse("okay")
 
 
-# @csrf_exempt
+
 # def Weather(request):
 #     queryset = WeatherInfo.objects.all()
-#     print("q",queryset)
 #     serializer = WeatherSerializer(queryset,many=True)
-#     print("ss",serializer)
 #     return JsonResponse(serializer.data,safe=False)
